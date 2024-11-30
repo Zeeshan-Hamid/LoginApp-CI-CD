634572623992650ab0fb6c3b02a42bf4e56cffa7
Zeeshan Hamid <zeeshanhamid17@gmail.com>
2024-12-01 02:47:25 +0500

    Another try

--- LoginApp.java
+++ LoginApp.java
@@ -80,7 +80,7 @@ public class LoginApp extends JFrame {
     SwingUtilities.invokeLater(() -> {
         LoginApp loginApp = new LoginApp();
         loginApp.setVisible(true);
-        System.out.println("Next version of CI/CD pipeline created");
+        System.out.println("Final version of CI/CD pipeline created");
     });
 }
 }
